4478a4ade36a5a5ed2e9a505dd1bfc08c3fc7176
hackamanda <hckamanda@gmail.com>
2022-05-21 23:05:52 -0300

    Adiciona todas as funcionalidades prontas

--- ViewController.swift
+++ ViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var LightSwitchLabel: UILabel!
+    
+    var lightsareON = true
+   
+    @IBAction func LightSwitchClickedButton(_ sender: Any) {
+        
+        if lightsareON == true {
+            LightSwitchLabel.text = "Lights ON"
+            LightSwitchLabel.textColor = UIColor.black
+            view.backgroundColor = UIColor.white
+            lightsareON.toggle()
+        } else {
+                lightsareON = false
+                LightSwitchLabel.text = "Lights OFF"
+            LightSwitchLabel.textColor = UIColor.white
+            view.backgroundColor = UIColor.black
+            lightsareON.toggle()
+            }
+        }
+    
     }
+    
 
 
-}
+
 
